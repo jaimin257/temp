81f00f3654a2be2ceff87d2a2bea6c3d13787b84
Jaimin <jaiminchaudhary42@gmail.com>
2020-03-24 10:35:19 +0530

    create tmp2.cpp
    created tmp2.cpp

---
+++ fitst/tmp2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin>>t;
+
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+
+        ll a[n];
+        for(ll i=0;i<n;i++) cin>>a[i];
+    }
+    return 0;
+}
